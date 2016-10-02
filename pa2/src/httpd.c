847df144016d4d88414182c3012fae47d4a1c7d2
Jan Hinrik Hansen <jan14@ru.is>
2016-10-02 21:24:37 +0000

    TCP example

--- pa2/src/httpd.c
+++ pa2/src/httpd.c
@@ -1,5 +1,3 @@
-/* your code goes here. */
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -10,31 +8,45 @@
 
 int main(int argc, char *argv[])
 {
-	int sockfd;
+    int sockfd;
     struct sockaddr_in server, client;
     char message[512];
 
-		/* Create and bind a UDP socket */
-	  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	  memset(&server, 0, sizeof(server));
-	  server.sin_family = AF_INET;
+    /* Create and bind a TCP socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     /* Network functions need arguments in network byte order instead of
-       host byte order. The macros htonl, htons convert the values. */
+ *        host byte order. The macros htonl, htons convert the values. */
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
-    server.sin_port = htons(32000);
-
+    server.sin_port = htons(atoi(argv[1]));
     bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
 
     /* Before the server can accept messages, it has to listen to the
-       welcome port. A backlog of one connection is allowed. */
+ *        welcome port. A backlog of one connection is allowed. */
+    listen(sockfd, 1);
 
     for (;;) {
         /* We first have to accept a TCP connection, connfd is a fresh
-           handle dedicated to this connection. */
+ *            handle dedicated to this connection. */
         socklen_t len = (socklen_t) sizeof(client);
-				recvfrom(sockfd, message, sizeof(message) - 1,
-				              0, (struct sockaddr *) &client, &len);
+        int connfd = accept(sockfd, (struct sockaddr *) &client, &len);
 
-          }
+        /* Receive from connfd, not sockfd. */
+        ssize_t n = recv(connfd, message, sizeof(message) - 1, 0);
+
+        message[n] = '\0';
+        fprintf(stdout, "Received:\n%s\n", message);
+
+        /* Convert message to upper case. */
+        for (int i = 0; i < n; ++i) message[i] = toupper(message[i]);
+
+        /* Send the message back. */
+        send(connfd, message, (size_t) n, 0);
+
+        /* Close the connection. */
+        shutdown(connfd, SHUT_RDWR);
+        close(connfd);
+    }
 }
